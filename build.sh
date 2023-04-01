affd96dca1cc5e00c8590669c74e7f21e8c35f0d
twardoch <adam@twardoch.com>
2023-04-01 15:36:08 +0200

    Changes reimplemented upstream, also updated copyright

--- build.sh
+++
@@ -1,6 +0,0 @@
-fontmake -m sources/Agdasima-Regular.designspace -o ttf --feature-writer None --output-dir fonts/ttf
-
-for ttf in fonts/ttf/*.ttf; do
-    gftools fix-font $ttf;
-    mv "$ttf.fix" $ttf;
-done
